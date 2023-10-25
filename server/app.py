c31117034422c0ec6995712b52abee7213901b52
Kerem Deen <kerem.deen@gmail.com>
2023-10-25 09:33:21 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,28 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake(id):
+    equake = Earthquake.query.filter(Earthquake.id == id).first()
+    if equake:
+        body = equake.to_dict()
+        status = 200
+    else:
+        body = {'message':f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_mag(magnitude):
+    quakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(quake.to_dict())
+    
+    body = {
+        'count': len(quakes),
+        'quakes': quakes
+        }
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
